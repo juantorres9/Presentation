f2e8f1a1ecbfed2a40084c3b8f565a25f299041d
TORRES ZETINO juan carlos <TORRES ZETINO juan carlos>
2017-01-11 12:37:23 +0100

    amelioration  structure repo suppresion MAPLAB

--- Programmes/MAPLAB/TP1-2regStatus/README.txt
+++
@@ -1,2 +0,0 @@
-STATUS : programme qui  test le different comporytements du bit C du STATUS REGISTRE
-pour l'ADDITION  et la SOUSTRACTION 
--- Programmes/MAPLAB/TP1Oper/E1OR.X/build/default/debug/EX1ORR.o.d
+++
@@ -1,4 +0,0 @@
-build/default/debug/EX1ORR.o : \
-	C:/Users/juantorres/TOTAL\ Projects/MAPLAB/TP1Operateurs/E1OR.X/EX1ORR.asm \
-	C:/Program\ Files\ (x86)/Microchip/MPLABX/mpasmx/P16F877.INC \
-
